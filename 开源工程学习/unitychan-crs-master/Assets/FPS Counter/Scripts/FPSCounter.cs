98a2797b864d75fe5a15cdbc822eff8b6c560d65
abc123zou123 <abc123zou123@163.com>
2019-08-31 16:21:25 +0800

    添加开源学习项目

---
+++ 开源工程学习/unitychan-crs-master/Assets/FPS Counter/Scripts/FPSCounter.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class FPSCounter : MonoBehaviour { 
+	 float updateInterval = 0.5f;
+ 
+	private float accum = 0.0f; // FPS accumulated over the interval
+	private float frames = 0f; // Frames drawn over the interval
+	private float timeleft  ; // Left time for current interval
+    //private ulong roleId = 9999;
+
+	void Start()
+	{
+		if( !this.GetComponent<Text>() )
+	    {
+	        print ("FramesPerSecond needs a GUIText component!");
+			this.enabled = false;
+	        return;
+	    }
+	    timeleft = updateInterval;  
+	}
+ 
+	void Update()
+	{
+	    timeleft -= Time.deltaTime;
+	    accum += Time.timeScale/Time.deltaTime;
+	    ++frames;
+
+//        if (GameUser.getInstance() != null && GameUser.getInstance().currentRole != null)
+//        {
+//            roleId = GameUser.getInstance().currentRole.roleId;
+//        }
+	 
+	    // Interval ended - update GUI text and start new interval
+	    if( timeleft <= 0.0f )
+	    {
+	        // display two fractional digits (f2 format)
+			this.GetComponent<Text>().text = "FPS - " + (accum/frames).ToString("f2");
+            //this.GetComponent<Text>().text += "\n"+"角色ID: " + roleId.ToString();
+	        timeleft = updateInterval;
+	        accum = 0.0f;
+	        frames = 0f;
+	    }
+	}
+}
---
+++ 开源工程学习/unitychan-crs-master/Assets/MyTest/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    bool isOpen = false;
+
+    public void OnCloseOrOpen(GameObject obj)
+    {
+       var allSrings = gameObject.GetComponentsInChildren<UnityChan.SpringManager>();
+        foreach(var spring in allSrings)
+        {
+            spring.enabled = isOpen;
+            
+        }
+        if (isOpen == false)
+        {
+            obj.GetComponent<UnityEngine.UI.Text>().text = "点击开启";
+        }
+        else
+        {
+            obj.GetComponent<UnityEngine.UI.Text>().text = "点击关闭";
+        }
+       
+        isOpen = !isOpen;
+    }
+}
